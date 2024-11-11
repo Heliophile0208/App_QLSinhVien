8c4af6e76c7e279ad4bb5bbfd3a2d4f261735cb8
Lê Thị Kim Ngân <lethikimngan20803@gmail.com>
2024-11-11 21:28:07 +0700

    Update _layout.js

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export default function _layout() {
   const router = useRouter();
-  const isLoggedIn = false; // Thay đổi điều này dựa trên trạng thái đăng nhập thực tế
+  const isLoggedIn = false; 
 
   useEffect(() => {
     if (!isLoggedIn) {
